2bb4c6666852fdebad12487e8868e7e68690cfcc
Jitesh <jitesh@jitesh-MacBook-Pro.local>
2023-12-16 02:15:44 +0530

    added OMDB API

--- omdbMovieApp/script .js
+++ omdbMovieApp/script .js
@@ -7,28 +7,26 @@ let container = document.getElementById("display");
 let getData = async () => {
 
     try{
-        let query = document.getElementById("movieSearch").value;
         
         let res = await fetch(`https://www.omdbapi.com/?i=tt1630029&apikey=${API_KEY}`);
 
         let data = await res.json();
-        // appendData(data);
-        console.log(data)
+        appendData(data);
+        // console.log(data)
     } catch(err){
         console.log(err)
     }
 };
-getData()
+// getData()
 
 let appendData = (data) => {
     container.innerHTML = null;
 
-    data.forEach(ele => {
         let imageDiv = document.createElement('div');
         imageDiv.setAttribute("id", "img");
 
-        let img = document.createElement('div');
-        img.src = ele.Poster;
+        let img = document.createElement('img');
+        img.src = data.Poster;
 
         imageDiv.append(img);
 
@@ -36,28 +34,27 @@ let appendData = (data) => {
         infoDiv.setAttribute("id", "info");
 
         let title = document.createElement('p');
-        title.innerText = `Tittle: ${ele.Title}`;
+        title.innerText = `Tittle: ${data.Title}`;
 
         let year = document.createElement('p');
-        year.innerText = `Year: ${ele.Year}`;
+        year.innerText = `Year: ${data.Year}`;
 
         let actor = document.createElement('p');
-        actor.innerText = `Actors: ${ele.Actors}`;
+        actor.innerText = `Actors: ${data.Actors}`;
 
         let writer = document.createElement('p');
-        writer.innerText = `Directors: ${ele.Director}`;
+        writer.innerText = `Directors: ${data.Director}`;
 
         let gener = document.createElement('p');
-        gener.innerText  = `Gener: ${ele.Genre}`;
+        gener.innerText  = `Gener: ${data.Genre}`;
 
         let plot = document.createElement('p');
-        plot.innerText = `Plot: ${ele.Plot}`;
+        plot.innerText = `Plot: ${data.Plot}`;
 
         let language = document.createElement('p');
-        language.innerText = `Language: ${ele.Language}`;
+        language.innerText = `Language: ${data.Language}`;
 
         infoDiv.append(title, year, actor, writer, gener, plot, language);
 
         container.append(imageDiv, infoDiv);
-    });
-}
+};
